98775d1546bce75ada6fcccd941a3645a50d8d99
Ashok Bakthavathsalam <kgashok@kgkite.ac.in>
2020-04-05 09:35:11 +0000

    added one more swap method

--- unit2/swap.py
+++ unit2/swap.py
@@ -5,18 +5,29 @@ a = int(input())
 b = int(input())
 
 # using a temporary variable
+print("Swapping using temporary variable...")
 t = a
 a = b
 b = t
 
 print("Value of a is {a} and\nValue of b is {b}".format(a=a, b=b))
 
-def swap():
+# using XOR operator, eliminating the need for 3rd variable
+print("Swapping using XOR operator...")
+a = a ^ b
+b = a ^ b
+a = a ^ b
+
+print("Value of a is {a} and\nValue of b is {b}".format(a=a, b=b))
+
+
+def swap_func():
     global a, b
-    print("Swap function called")
+    print("swap_func function called")
     a, b = b, a
-    
-swap()  # go back to their original values
+
+
+swap_func()  # go back to their original values
 print("Value of a is {a} and\nValue of b is {b}".format(a=a, b=b))
 
 
