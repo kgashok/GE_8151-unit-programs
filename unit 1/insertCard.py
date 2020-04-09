525b251009958f5764f7688e68ec3329ff548840
Peter Evans <peter-evans@users.noreply.github.com>
2020-04-09 18:16:48 +0000

    autopep8 action fixes

--- unit 1/insertCard.py
+++ unit 1/insertCard.py
@@ -1,22 +1,24 @@
-#insert a card in a list of sorted cards
+# insert a card in a list of sorted cards
 
-order = { 
-'A': 1, '2': 2, '3': 3, '4': 4, 
-'5': 5, '6': 6, '7': 7, '8': 8, 
-'9': 9, '10': 10, 
-'J': 11, 'Q': 12, 'K': 13 
-} 
+order = {
+    'A': 1, '2': 2, '3': 3, '4': 4,
+    '5': 5, '6': 6, '7': 7, '8': 8,
+    '9': 9, '10': 10,
+    'J': 11, 'Q': 12, 'K': 13
+}
 
-def insertCard(deck, newCard): 
-    for card in deck: 
-        if order[card] > order[newCard]: 
-             index = deck.index(card) 
-             deck.insert(index, newCard) 
-             break 
+
+def insertCard(deck, newCard):
+    for card in deck:
+        if order[card] > order[newCard]:
+            index = deck.index(card)
+            deck.insert(index, newCard)
+            break
     return deck
 
-deck = ['2', '5', '8', '10', 'J', 'K']  #initial set of cards
+
+deck = ['2', '5', '8', '10', 'J', 'K']  # initial set of cards
 print("deck = ", deck)
-newCard = input("Enter the new card to be inserted:")  #get the new card
-insertCard(deck, newCard) 
+newCard = input("Enter the new card to be inserted:")  # get the new card
+insertCard(deck, newCard)
 print(deck)
