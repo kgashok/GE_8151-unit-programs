ebcd162fc1355920c8b2ef7b595ec7f998605bea
Ashok Bakthavathsalam <kgashok@users.noreply.github.com>
2021-09-17 04:35:19 +0530

    big random list generation

--- unit3/binary.py
+++ unit3/binary.py
@@ -3,7 +3,7 @@ import random
 
 
 def bsearch_slice(alist, token):
-    '''binary search using the intuitive slice operator
+    '''binary chop (aka binary search) using the intuitive slice operator
     @author kgashok
     @param alist is a list of numbers
     @param token is an integer
@@ -11,19 +11,20 @@ def bsearch_slice(alist, token):
     @return True if found else False 
 
     '''
+    c = 0
     while alist:
         print(f'{alist}', end=":: ")
         midpoint = len(alist) // 2
         midvalue = alist[midpoint]
 
-        print(f'search for: {token} at mid:{midpoint}, midvalue: {midvalue}')
+        print(f'Try {c} : search for: {token} at mid:{midpoint}, midvalue: {midvalue}')
 
         if token == midvalue:
             return True
 
         alist = alist[:midpoint] if token < midvalue else \
             alist[midpoint+1:]
-
+        c += 1
     return False
 
 
@@ -40,7 +41,7 @@ def binary_search(alist, token):
     '''
     left = 0
     right = len(alist)
-    print("alist: {0}, search for {1}".format(alist, token))
+    # print("alist: {0}, search for {1}".format(alist, token))
 
     while left < right:
         midpoint = (left + right) // 2
@@ -61,22 +62,17 @@ def binary_search(alist, token):
 # Main Program
 num_list = []
 n = int(input("Enter the number of elements: "))
-if n == 0:
-    num_list = sorted([random.randint(1, 100) for _ in range(10)])
-    num_list.sort()
-    print(num_list)
+num_list = sorted([random.randint(1, n*10) for _ in range(n)])
+num_list.sort()
+print(num_list[:10], "...", num_list[-1:])
+
+'''
 else:
     for i in range(0, n):
         item = input("Enter the item:")
         num_list.append(int(item))
-
+'''
 search = int(input("Enter element to search: "))
-print("****Binary Search using Slice")
-ans = bsearch_slice(num_list, search)
-if ans:
-    print("Element {0} Found".format(search))
-else:
-    print("Element {0} not found!".format(search))
 
 print("****Binary Search using indexes")
 ans = binary_search(num_list, search)
@@ -84,3 +80,11 @@ if ans != -1:
     print("Element {0} Found at index {1}".format(search, ans))
 else:
     print("Element {0} not found!".format(search))
+
+print("****Binary Search using Slice")
+ans = bsearch_slice(num_list, search)
+if ans:
+    print("Element {0} Found".format(search))
+else:
+    print("Element {0} not found!".format(search))
+
