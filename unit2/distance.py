10e9a04e5094900aa2f39685c8af84ef9dc4f9c8
Ashok Bakthavathsalam <kgashok@users.noreply.github.com>
2021-09-18 08:08:23 +0530

    added docstring to distance

--- unit2/distance.py
+++ unit2/distance.py
@@ -4,6 +4,14 @@ from math import sqrt
 
 
 def distance_between(pointA, pointB):
+    '''returns the distance between two cartesian points
+    
+    @author kgashok
+    @param pointA tuple containing x and y coordinates
+    @param pointB tuple containing x and y coordinates
+    
+    @return float value representing distance'''
+    
     x1, y1 = pointA
     x2, y2 = pointB
 
@@ -16,10 +24,10 @@ def distance_between(pointA, pointB):
 # main program
 # tuple packing allows for assigning
 # multiple inputs in one statement
-pointA = int(input("Enter xcordinate for PointA ")), \
-    int(input("Enter ycordinate for PointA "))
+pointA = int(input("Enter x_cordinate for PointA ")), \
+    int(input("Enter y_cordinate for PointA "))
 
-pointB = int(input("Enter xcordinate for PointB ")), \
-    int(input("Enter ycordinate for PointB "))
+pointB = int(input("Enter x_cordinate for PointB ")), \
+    int(input("Enter y_cordinate for PointB "))
 
 print("distance between points", distance_between(pointA, pointB))
