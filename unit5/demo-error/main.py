6bb91175d337f508becec1fdd8d5b1732788e679
Ashok Bakthavathsalam <kgashok@users.noreply.github.com>
2021-10-15 02:04:12 +0530

    minor edit to fix build error

--- unit5/demo-error/main.py
+++ unit5/demo-error/main.py
@@ -1,5 +1,10 @@
 print("\n DEMO ERRORS")
 print("------------------")
 a = 20
+
+'''Original code by rajasekaranap, fails the build
 if a < 30
 print("a is less than 30")
+'''
+if a < 30:
+    print("a is less than 30")
